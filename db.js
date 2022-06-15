4d3677043c8f4e40bd042aff7581cfaca2017f14
Yao Yu <amyyuyao.thu@gmail.com>
2022-06-14 23:07:01 -0400

    fix

--- db.js
+++ db.js
@@ -5,8 +5,8 @@ const Promise = require('bluebird');
 
 // const connection = new Sequelize('postgres://localhost:5432/postgres');
 
-const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+const connection = new Sequelize('postgres', 'postgres', '5555', {
+  host: '52.91.243.247',
   dialect: 'postgres',
   logging: false,
 });
