2a52c87c27e5197938f836e9e4ff2350b6ed67c3
Yao Yu <amyyuyao.thu@gmail.com>
2022-06-14 20:44:34 -0400

    change db url

--- db.js
+++ db.js
@@ -4,13 +4,10 @@ const Promise = require('bluebird');
 
 // const connection = new Sequelize('postgres://52.91.243.247:5432/postgres');
 
-const connection = new Sequelize({
+const sequelize = new Sequelize('postgres', 'postgres', '5555', {
   host: '52.91.243.247',
-  port: 5432,
-  user: 'postgres',
-  password: '5555',
-  database: 'postgres'
-})
+  dialect: 'postgres'
+});
 
 connection.authenticate()
   .then(() => console.log('Connected to PostgreSQL db!'))
