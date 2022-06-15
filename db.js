d2c9c383cc1517a52738c409a8c1371db28d10cb
Yao Yu <amyyuyao.thu@gmail.com>
2022-06-14 20:32:24 -0400

    change db url

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const axios = require('axios');
 const Promise = require('bluebird');
 
-const connection = new Sequelize('postgres://localhost:5432/postgres');
+const connection = new Sequelize('postgres://52.91.243.247:5432/postgres');
 connection.authenticate()
   .then(() => console.log('Connected to PostgreSQL db!'))
   .catch((err) => console.log('Unable to connect to  PostgreSQL db: ', err));
